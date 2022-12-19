3617383db808592a6983934d515e1e41921e38a3
nattthebear <goyuken@gmail.com>
2022-12-19 17:46:24 -0500

    pass some more tests

--- src/solver/Dictionary.test.ts
+++ src/solver/Dictionary.test.ts
@@ -35,8 +35,27 @@ describe("Dictionary", () => {
 	});
 
 	it("pivot test 2", () => {
-		// const d = parse("4,5,6;1,2,3,0;4,-2,1,-2,1,-5,-2,3,-1,1,-1,1,-1,2,1,0,0,0,0,-1");
-		// assert(d);
-		// const p1 = pivot(d, true);
+		// https://www.matem.unam.mx/~omar/math340/2-phase.html
+		const d = parse("4,5,6;1,2,3,0;4,-2,1,-2,1,-5,-2,3,-1,1,-1,1,-1,2,1,0,0,0,0,-1");
+		assert(d);
+
+		const p1 = pivot(d, true);
+		assert(p1);
+		assert.equal(
+			stringify(p1),
+			"0,4,6;1,2,3,5;5:1,2:1,-3:1,1:1,1:1,9:1,0:1,-2:1,-1:1,1:1,4:1,3:1,-4:1,3:1,1:1,-5:1,-2:1,3:1,-1:1,-1:1"
+		);
+
+		const p2 = pivot(p1, false);
+		assert(p2);
+		assert.equal(
+			stringify(p2),
+			"2,0,4;1,3,5,6;1:1,3:4,3:4,1:4,-1:4,2:1,-1:4,-5:4,1:4,3:4,7:1,-3:2,-5:2,1:2,1:2,-2:1,1:4,5:4,-1:4,-3:4"
+		);
+
+		const p3 = pivot(p2, false);
+		assert(p3);
+		//
+		// assert.equal(stringify(p3), "3,2,4;1,5,6,0;");
 	});
 });
