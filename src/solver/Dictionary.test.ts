e84c8cc15df5a646d08488436b16fdf6fe3572ed
nattthebear <goyuken@gmail.com>
2022-12-19 17:33:13 -0500

    some first steps on our own simplex solver

---
+++ src/solver/Dictionary.test.ts
@@ -0,0 +1,42 @@
+import { describe, it } from "node:test";
+import assert from "node:assert/strict";
+
+import { Dictionary, parse, pivot, stringify } from "./Dictionary";
+
+describe("Dictionary", () => {
+	it("pivot test 1", () => {
+		// https://www.matem.unam.mx/~omar/math340/simplex-intro.html
+		const d = parse("4,5,6;1,2,3;14,-2,-1,-1,28,-4,-2,-3,30,-2,-5,-5,0,1,2,-1");
+		assert(d);
+		assert.equal(
+			stringify(d),
+			"4,5,6;1,2,3;14:1,-2:1,-1:1,-1:1,28:1,-4:1,-2:1,-3:1,30:1,-2:1,-5:1,-5:1,0:1,1:1,2:1,-1:1"
+		);
+
+		const p1 = pivot(d, false);
+		assert(p1);
+		assert.equal(
+			stringify(p1),
+			"2,4,5;1,3,6;6:1,-2:5,-1:1,-1:5,8:1,-8:5,0:1,1:5,16:1,-16:5,-1:1,2:5,12:1,1:5,-3:1,-2:5"
+		);
+
+		const p2 = pivot(p1, false);
+		assert(p2);
+		// Omar's solution here is:
+		// "1,2,5;3,4,6;5:1,0:1,-5:8,1:8,4:1,-1:1,1:4,-1:4,0:1,-1:1,2:1,0:1,13:1,-3:1,-1:8,-3:8"
+		// But it's a just an ordering difference -- he's kept the 3,4,6 in ascending order where there's no particular need to.
+		assert.equal(
+			stringify(p2),
+			"1,2,5;3,6,4;5:1,0:1,1:8,-5:8,4:1,-1:1,-1:4,1:4,0:1,-1:1,0:1,2:1,13:1,-3:1,-3:8,-1:8"
+		);
+
+		const p3 = pivot(p2, false);
+		assert(!p3);
+	});
+
+	it("pivot test 2", () => {
+		// const d = parse("4,5,6;1,2,3,0;4,-2,1,-2,1,-5,-2,3,-1,1,-1,1,-1,2,1,0,0,0,0,-1");
+		// assert(d);
+		// const p1 = pivot(d, true);
+	});
+});
---
+++ src/solver/Dictionary.ts
@@ -0,0 +1,194 @@
+import { BigRat } from "../math/BigRat";
+
+// We proceed as in https://www.matem.unam.mx/~omar/math340
+
+export interface Dictionary {
+	/** names of the exiting variables, top to bottom */
+	basic: number[];
+	/** names of the entering variables, left to right */
+	nonBasic: number[];
+	/** All coefficients, left to right then top to bottom */
+	coefficients: BigRat[];
+}
+
+/** Number of columns */
+const cols = (d: Dictionary) => d.nonBasic.length + 1;
+/** Number of rows */
+const rows = (d: Dictionary) => d.basic.length + 1;
+
+export const stringify = (d: Dictionary) =>
+	`${d.basic.join()};${d.nonBasic.join()};${d.coefficients.map((c) => c.toRatioString()).join()}`;
+
+export function parse(s: string): Dictionary | null {
+	const parts = s.split(";");
+	if (parts.length !== 3) {
+		return null;
+	}
+	function parsePart<T>(t: string, validator: RegExp, project: (v: string) => T): T[] | null {
+		const tt = t.split(",");
+		const res: T[] = [];
+		for (const v of tt) {
+			if (!v.match(validator)) {
+				return null;
+			}
+			res.push(project(v));
+		}
+		return res;
+	}
+
+	const basic = parsePart(parts[0], /^\d+$/, (v) => parseInt(v, 10));
+	const nonBasic = parsePart(parts[1], /^\d+$/, (v) => parseInt(v, 10));
+	const coefficients = parsePart(parts[2], /^-?\d+(:-?\d+)?$/, (v) => {
+		const split = v.split(":");
+		return new BigRat(BigInt(split[0]), BigInt(split[1] ?? "1"));
+	});
+	if (!basic || !nonBasic || !coefficients) {
+		return null;
+	}
+	return { basic, nonBasic, coefficients };
+}
+
+function isFeasible(dict: Dictionary) {
+	const pitch = cols(dict);
+	const max = pitch * rows(dict);
+	const { coefficients } = dict;
+	for (let a = 0; a < max; a += pitch) {
+		if (coefficients[a].sign() < 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+export function pivot(dict: Dictionary, special: boolean): Dictionary | null {
+	const pitch = cols(dict);
+	const rowCount = rows(dict);
+	const coord = (i: number, j: number) => i + j * pitch;
+	const max = pitch * rowCount;
+	const { basic, nonBasic, coefficients } = dict;
+
+	let enterCol = -1;
+	let enterName = -1;
+	if (!special) {
+		let enterCoeff: BigRat | undefined;
+		for (let a = coord(1, rowCount - 1), idx = 0; a < max; a++, idx++) {
+			const coeff = coefficients[a];
+			if (coeff.sign() <= 0) {
+				continue;
+			}
+			const varName = nonBasic[idx];
+			const cmp = enterCoeff && BigRat.compare(enterCoeff, coeff);
+			if (cmp == null || cmp === -1 || (cmp === 0 && varName > enterName)) {
+				enterCol = idx + 1;
+				enterName = varName;
+				enterCoeff = coeff;
+			}
+		}
+		if (!enterCoeff) {
+			return null;
+		}
+	} else {
+		enterCol = pitch - 1;
+		enterName = 0;
+	}
+
+	let exitRow = -1;
+	let exitName = -1;
+	if (!special) {
+		let exitCoeff: BigRat | undefined;
+		for (let a = coord(enterCol, 0), b = 0, idx = 0; a < max - pitch; a += pitch, b += pitch, idx++) {
+			const coeff = coefficients[b].div(coefficients[a]).neg();
+			const varName = basic[idx];
+			const cmp = exitCoeff && BigRat.compare(exitCoeff, coeff);
+			if (cmp == null || cmp === 1 || (cmp === 0 && varName < exitName)) {
+				exitRow = idx;
+				exitName = varName;
+				exitCoeff = coeff;
+			}
+		}
+		if (!exitCoeff) {
+			return null;
+		}
+	} else {
+		const stop = max - pitch;
+		let exitCoeff: BigRat | undefined;
+		for (let a = 0, idx = 0; a < stop; a += pitch, idx++) {
+			const coeff = coefficients[a];
+			const varName = basic[idx];
+			if (coeff.sign() > 0) {
+				continue;
+			}
+			const cmp = exitCoeff && BigRat.compare(exitCoeff, coeff);
+			if (cmp == null || cmp === 1 || (cmp === 0 && varName < exitName)) {
+				exitRow = idx;
+				exitName = varName;
+				exitCoeff = coeff;
+			}
+		}
+		if (!exitCoeff) {
+			return null;
+		}
+	}
+
+	const newNonBasic = nonBasic.slice();
+	newNonBasic.splice(enterCol - 1, 1);
+	newNonBasic.push(exitName);
+
+	const newBasic = basic.slice();
+	newBasic.splice(exitRow, 1);
+	newBasic.unshift(enterName);
+
+	const newCoefficients = Array<BigRat>(max);
+
+	const aExit = exitRow * pitch;
+	let aTo = 0;
+	{
+		let aFrom = aExit;
+		const pivotIndex = aFrom + enterCol;
+		const rowEnd = aFrom + pitch;
+		const pivotValue = coefficients[pivotIndex].neg();
+		for (; aTo < pitch; aTo++, aFrom++) {
+			if (aFrom === pivotIndex) {
+				aFrom++;
+			}
+			let value = aFrom < rowEnd ? coefficients[aFrom] : BigRat.MINUS_ONE;
+			value = value.div(pivotValue);
+			newCoefficients[aTo] = value;
+		}
+	}
+
+	for (let aFrom = 0; aFrom < max; aFrom += pitch) {
+		if (aFrom === aExit) {
+			continue;
+		}
+		const pivotIndex = aFrom + enterCol;
+		const pivotValue = coefficients[pivotIndex];
+		const rowEnd = aFrom + pitch;
+		for (let aTop = 0, aRow = aFrom; aTop < pitch; aTop++, aRow++, aTo++) {
+			if (aRow === pivotIndex) {
+				aRow++;
+			}
+			const top = newCoefficients[aTop];
+			const bottom = aRow < rowEnd ? coefficients[aRow] : BigRat.ZERO;
+			newCoefficients[aTo] = top.mul(pivotValue).add(bottom);
+		}
+	}
+
+	return {
+		basic: newBasic,
+		nonBasic: newNonBasic,
+		coefficients: newCoefficients,
+	};
+}
+
+export function pivotSpecial(dict: Dictionary): Dictionary | null {
+	const pitch = cols(dict);
+	const rowCount = rows(dict);
+	const coord = (i: number, j: number) => i + j * pitch;
+	const max = pitch * rowCount;
+	const { basic, nonBasic, coefficients } = dict;
+
+	return null;
+}
+
+export {};
